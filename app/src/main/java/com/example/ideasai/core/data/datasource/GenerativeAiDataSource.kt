112f70118ed3282adb802571e0c332a303f50b2d
Joko Triyanto <jokotriyanto12345@gmail.com>
2025-01-30 10:20:59 +0700

    refactor(datasource): update GenerativeAiDataSourceImpl

--- app/src/main/java/com/example/ideasai/core/data/datasource/GenerativeAiDataSource.kt
+++ app/src/main/java/com/example/ideasai/core/data/datasource/GenerativeAiDataSource.kt
@@ -1,10 +1,10 @@
 package com.example.ideasai.core.data.datasource
 
-import android.util.Log
 import com.example.ideasai.core.data.DataResult
 import com.example.ideasai.core.data.model.Idea
 import com.example.ideasai.core.data.model.IdeaResponse
 import com.example.ideasai.core.data.model.toDomain
+import com.example.ideasai.core.util.cleanGeminiJsonResponse
 import com.google.ai.client.generativeai.GenerativeModel
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
@@ -27,30 +27,32 @@ class GenerativeAiDataSourceImpl @Inject constructor(
         topics: String,
         lang: String
     ): DataResult<List<Idea>> {
-        val jsonFormat = "[\n" +
+
+        return try {
+            val prompt =
+                "I'm $about, Generate me a list of ideas for this purpose: $purpose, based on these topics: $topics, in $lang. Make the output in this JSON format: $JSON_FORMAT"
+
+            val response = generativeModel.generateContent(prompt)
+            val json = Json {
+                ignoreUnknownKeys = true
+            }
+
+            val ideas: List<Idea> = json.decodeFromString<List<IdeaResponse>>(
+                response.text.orEmpty().cleanGeminiJsonResponse()
+            ).map { it.toDomain() }
+
+            DataResult.Success(ideas)
+        } catch (e: Exception) {
+            DataResult.Error(e.localizedMessage.orEmpty())
+        }
+    }
+
+    companion object {
+        const val JSON_FORMAT = "[\n" +
                 "  {\n" +
                 "    \"topic\": \"\",\n" +
                 "    \"name\": \"\"\n" +
                 "  }\n" +
                 "]\n"
-
-        return try {
-            val prompt =
-                "I'm $about, Generate me a list of ideas for this purpose: $purpose, based on these topics, in $lang: $topics. Make the output in this JSON format: $jsonFormat"
-
-            val response = generativeModel.generateContent(prompt)
-            var text = response.text.orEmpty()
-            Log.d("generate_before:", "$text")
-            text = text.replace("```json", "").replace("```", "")
-            Log.d("generate_after:", "$text")
-            val json = Json {
-                ignoreUnknownKeys = true
-            }
-            val ideas: List<Idea> = json.decodeFromString<List<IdeaResponse>>(text)
-                    .map { it.toDomain() }
-            DataResult.Success(ideas)
-        } catch (e: Exception) {
-            DataResult.Error(e.localizedMessage.orEmpty())
-        }
     }
 }
